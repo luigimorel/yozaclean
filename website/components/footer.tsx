2cb0a3b50aecd767e67cf2ca11ebca8286d7a14d
Luigi Morel <morelmiles@gmail.com>
2022-11-17 17:29:54 +0300

    changes

--- website/components/footer.tsx
+++ website/components/footer.tsx
@@ -25,6 +25,7 @@ const Facebook = ({ size = 24 }) => (
     <path d="M24 12.07C24 5.41 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.8-4.7 4.54-4.7 1.31 0 2.68.24 2.68.24v2.97h-1.5c-1.5 0-1.96.93-1.96 1.89v2.26h3.32l-.53 3.5h-2.8V24C19.62 23.1 24 18.1 24 12.07" />
   </svg>
 );
+
 const Instagram = ({ size = 24 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -137,6 +138,7 @@ export default function Footer() {
               ))}
             </div>
           </div>
+
           <div>
             <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
               {legal.map((item, index) => (
@@ -148,6 +150,7 @@ export default function Footer() {
               ))}
             </div>
           </div>
+
           <div className="">
             <div>Follow us</div>
             <div className="flex justify-center xl:justify-start mt-5 space-x-10 text-gray-400 dark:text-gray-500">
